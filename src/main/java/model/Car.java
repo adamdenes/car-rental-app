1c06523d9fb3470f1ad877093fd32aa3b5e97d54
adamdenes <adam.denes@icloud.com>
2023-04-23 13:14:48 +0200

    feat: adding Car class for hibernate

---
+++ src/main/java/model/Car.java
@@ -0,0 +1,32 @@
+package model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "carpool")
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(nullable = false, name = "make")
+    private String make;
+    @Column(nullable = false, name = "model")
+    private String model;
+    @Column(nullable = false, name = "year")
+    private int year;
+    @Column(nullable = false,  name = "rental start")
+    private LocalDate rentalStartDate;
+    @Column(nullable = false,  name = "rental end")
+    private LocalDate rentalEndDate;
+}
