240b9a9b76f5cfbf2a0c1a6a9fd80b573b0e48ee
adamdenes <adam.denes@icloud.com>
2023-04-25 21:29:56 +0200

    feat: finished `CarDao` unit tests

--- src/test/java/hu/unideb/inf/CarDaoTest.java
+++ src/test/java/hu/unideb/inf/CarDaoTest.java
@@ -2,16 +2,14 @@ package hu.unideb.inf;
 
 import model.CarRentalModel;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 import static controller.CarRentalController.jdbi;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CarDaoTest {
 
@@ -35,11 +33,13 @@ public class CarDaoTest {
     }
 
     @Test
+    @DisplayName("Test createTable method")
     void testCreateTable() {
         assertTrue(dao.checkTableExists("test_table"));
     }
 
     @Test
+    @DisplayName("Test insertCar and getCarByPlate methods")
     public void testCreateAndRetrieveCar() {
         CarRentalModel car = new CarRentalModel("ABC123", "Toyota", "Camry", 2020, null, CarRentalModel.State.AVAILABLE);
         dao.insertCar("test_table", car);
@@ -47,6 +47,49 @@ public class CarDaoTest {
         Optional<CarRentalModel> retrievedCar = dao.getCarByPlate("test_table", "ABC123");
         assertTrue(retrievedCar.isPresent());
         assertEquals(car, retrievedCar.get());
+    }
+
+    @Test
+    @DisplayName("Test deleteCarByPlate method")
+    public void testDeleteCarByPlate() {
+        CarRentalModel car = new CarRentalModel("ABC-123", "Opel", "Astra", 2010,
+                LocalDate.of(2022, 4, 25), CarRentalModel.State.AVAILABLE);
+
+        dao.insertCar("test_table", car);
+        dao.deleteCarByPlate("test_table", "ABC-123");
+
+        Optional<CarRentalModel> actualCar = dao.getCarByPlate("test_table", "ABC-123");
+        assertFalse(actualCar.isPresent());
+    }
+
+    @Test
+    @DisplayName("Test updateCar method")
+    public void testUpdateCar() {
+        CarRentalModel car = new CarRentalModel("ABC-123", "Opel", "Astra", 2010,
+                LocalDate.of(2022, 4, 25), CarRentalModel.State.AVAILABLE);
+        dao.insertCar("test_table", car);
+
+        CarRentalModel updatedCar = new CarRentalModel("ABC-123", "Opel", "Astra", 2010,
+                LocalDate.of(2022, 4, 26), CarRentalModel.State.RENTED);
+        dao.updateCar("test_table", updatedCar);
+
+        Optional<CarRentalModel> actualCar = dao.getCarByPlate("test_table", "ABC-123");
+        assertNotEquals(car.getRentalStartDate(), actualCar.get().getRentalStartDate());
+        assertNotEquals(car.getState(), actualCar.get().getState());
+        assertEquals(LocalDate.of(2022, 4, 26), actualCar.get().getRentalStartDate());
+        assertEquals(CarRentalModel.State.RENTED, actualCar.get().getState());
+    }
+
+    @Test
+    @DisplayName("Test deleteAllCars method")
+    public void testDeleteAllCars() {
+        dao.insertCar("test_table", new CarRentalModel("AB1234", "Ford", "Mustang", 2022, null, CarRentalModel.State.AVAILABLE));
+        dao.insertCar("test_table", new CarRentalModel("CD5678", "Tesla", "Model 3", 2021, null, CarRentalModel.State.RENTED));
+
+        dao.deleteAllCars("test_table");
+
+        List<CarRentalModel> cars = dao.getCars("test_table");
+        assertEquals(0, cars.size());
     }
 
 }
