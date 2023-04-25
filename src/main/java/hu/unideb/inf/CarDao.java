809853a42a2ce6fd5cc5094420fd891e665d3f94
adamdenes <adam.denes@icloud.com>
2023-04-25 20:57:09 +0200

    feat: rewriting CarDao for testing - table name is a parameter now

--- src/main/java/hu/unideb/inf/CarDao.java
+++ src/main/java/hu/unideb/inf/CarDao.java
@@ -1,10 +1,11 @@
-package hu.inf.unideb;
+package hu.unideb.inf;
 
 
 import model.CarRentalModel;
 import org.jdbi.v3.sqlobject.config.RegisterBeanMapper;
 import org.jdbi.v3.sqlobject.customizer.Bind;
 import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.customizer.Define;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
@@ -14,50 +15,47 @@ import java.util.Optional;
 
 @RegisterBeanMapper(CarRentalModel.class)
 public interface CarDao {
-    @SqlUpdate("""
-            CREATE TABLE carpool (
-                "plate" VARCHAR PRIMARY KEY,
-                "make" VARCHAR NOT NULL,
-                "model" VARCHAR NOT NULL,
-                year INTEGER NOT NULL,
-                "rentalStartDate" INTEGER,
-                "state" VARCHAR NOT NULL
-            )
-            """
-    )
-    void createTable();
+    @SqlUpdate("CREATE TABLE IF NOT EXISTS <tableName> ("
+            + "plate VARCHAR PRIMARY KEY, "
+            + "make VARCHAR NOT NULL, "
+            + "model VARCHAR NOT NULL, "
+            + "year INTEGER NOT NULL, "
+            + "rentalStartDate INTEGER, "
+            + "state VARCHAR NOT NULL)")
+    void createTable(@Define("tableName") String tableName);
 
-    @SqlUpdate("DROP TABLE carpool;")
-    void dropTable();
+    @SqlUpdate("DROP TABLE IF EXISTS <tableName>")
+    void dropTable(@Define("tableName") String tableName);
 
-    @SqlQuery("SELECT count(*) FROM sqlite_master WHERE type='table' AND name='carpool'")
-    boolean checkTableExists();
+    @SqlQuery("SELECT count(*) FROM sqlite_master WHERE type='table' AND name=:tableName")
+    boolean checkTableExists(@Bind("tableName") String tableName);
 
-    @SqlUpdate("INSERT INTO carpool VALUES (:plate, :make, :model, :year, :rentalStartDate, :state)")
-    void insertCar(@Bind("plate") String plate,
+    @SqlUpdate("INSERT INTO <tableName> VALUES (:plate, :make, :model, :year, :rentalStartDate, :state)")
+    void insertCar(@Define("tableName") String tableName,
+                   @Bind("plate") String plate,
                    @Bind("make") String make,
                    @Bind("model") String model,
                    @Bind("year") int year,
                    @Bind("rentalStartDate") LocalDate rentalStartDate,
                    @Bind("state") CarRentalModel.State state);
 
-    @SqlUpdate("INSERT INTO carpool VALUES (:plate, :make, :model, :year, :rentalStartDate, :state)")
-    void insertCar(@BindBean CarRentalModel car);
+    @SqlUpdate("INSERT INTO <tableName> VALUES (:plate, :make, :model, :year, :rentalStartDate, :state)")
+    void insertCar(@Define("tableName") String tableName, @BindBean CarRentalModel car);
 
-    @SqlQuery("SELECT * FROM carpool WHERE plate = :plate")
-    Optional<CarRentalModel> getCarByPlate(@Bind("plate") String plate);
+    @SqlQuery("SELECT * FROM <tableName> WHERE plate = :plate")
+    Optional<CarRentalModel> getCarByPlate(@Define("tableName") String tableName, @Bind("plate") String plate);
 
-    @SqlQuery("SELECT * FROM carpool ORDER BY plate")
-    List<CarRentalModel> getCars();
+    @SqlQuery("SELECT * FROM <tableName> ORDER BY plate")
+    List<CarRentalModel> getCars(@Define("tableName") String tableName);
 
-    @SqlUpdate("DELETE FROM carpool")
-    void deleteAllCars();
+    @SqlUpdate("DELETE FROM <tableName>")
+    void deleteAllCars(@Define("tableName") String tableName);
 
-    @SqlUpdate("DELETE FROM carpool WHERE plate = :plate")
-    void deleteCarByPlate(@Bind("plate") String plate);
+    @SqlUpdate("DELETE FROM <tableName> WHERE plate = :plate")
+    void deleteCarByPlate(@Define("tableName") String tableName, @Bind("plate") String plate);
 
 
-    @SqlUpdate("UPDATE carpool SET make = :make, model = :model, year = :year, " +
-            "rentalStartDate = :rentalStartDate, state = :state WHERE plate = :plate")
-    void updateCar(@BindBean CarRentalModel car);
+    @SqlUpdate("UPDATE <tableName> SET make = :make, model = :model, year = :year, "
+            + "rentalStartDate = :rentalStartDate, state = :state WHERE plate = :plate")
+    void updateCar(@Define("tableName") String tableName, @BindBean CarRentalModel car);
 }
