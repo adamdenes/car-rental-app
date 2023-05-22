7ff082174dac5149d8b54ab17a60409fc6d37cce
adamdenes <adam.denes@icloud.com>
2023-05-22 09:17:36 +0200

    pointing JDBI import to the model

--- src/test/java/hu/unideb/inf/CarDaoTest.java
+++ src/test/java/hu/unideb/inf/CarDaoTest.java
@@ -8,7 +8,7 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
-import static controller.CarRentalController.jdbi;
+import static model.CarRentalModel.jdbi;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CarDaoTest {
